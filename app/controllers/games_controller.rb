ae96f8b20b5f0c1f65cda33596b4084f9fa2d99c
Breno Moura <basmoura@gmail.com>
2014-05-11 14:58:15 -0300

    Adiciona controller de jogos

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,52 @@
+class GamesController < ApplicationController
+  before_action :set_game, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @games = Game.order(:dt_game)
+  end
+
+  def show
+  end
+
+  def new
+    @game = Game.new
+  end
+
+  def create
+    @game = Game.new(game_params)
+
+    if @game
+      redirect_to games_path
+      flash[:success] = "Jogo cadastrado com sucesso"
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @game.update(game_params)
+      redirect_to games_path
+      flash[:success] = "Jogo atualizado com sucesso"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @game.destroy
+    redirect_to games_path
+    flash[:success] = "Jogo removido com sucesso"
+  end
+
+  private
+  def set_game
+    @game = Game.find(params[:id])
+  end
+
+  def game_params
+    params.require(:game).permit(:team_a, :team_b, :score_ta, :score_tb)
+  end
+end
