234cdccd808e90f6b0424dd203de9f5f901d0481
Breno Moura <basmoura@gmail.com>
2014-05-15 16:20:11 -0300

    Adiciona inflection da api e altera rotas

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  root "games#index"
+
   resources :games
+  resources :users, only: :show
+  resources :bet, only: :show
+
+  namespace :api, defaults: { format: 'json' } do
+    resources :users, only: [:index, :create]
+    resources :bets, only: [:index, :create]
+    resources :games, only: [:index]
+  end
 end
